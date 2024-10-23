9ce8079555b03a2ea812208dd3a1452f54eac69c
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-10-23 06:01:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import Vapi from "@vapi-ai/web";
+import "./index.css";
 
-const vapi = new Vapi("bb01b4a0-3597-4ef3-a3c7-f777014ca78f");
-vapi.start("5439b123-43bd-4983-b794-c1b404273d72");
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// render application
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
